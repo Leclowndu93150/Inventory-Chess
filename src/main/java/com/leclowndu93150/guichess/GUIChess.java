e900b3c122a38fc654da8d7c056d26fb6d90754e
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-06-02 21:05:12 +0200

    yaye

--- src/main/java/com/leclowndu93150/guichess/GUIChess.java
+++ src/main/java/com/leclowndu93150/guichess/GUIChess.java
@@ -53,4 +53,22 @@ public class GUIChess {
     //TODO: don't delete player's inventories when in a game or anything, just save it until game ended
     //TODO: don't allow the king to move if it's going to put it in a check state
     //TODO: also fix the check system and checkmate
+//    Add Stockfish support
+//    Can request up to 3 hints per game (configurable in the challenge menu)
+//
+//    Will make a new gui, simple chest, allows you to chose if against a bot or a player, let's you chose the mode, time, amount of hints, if against a bot it's elo. If it's either random or chosen sides. Other settings
+//
+//    Elo will probably use materials like diamond etc, hints will use the light level light bulbs item, with 0, 1, 2 or 3.
+//
+//    Being able to bet items also through a GUI
+//
+//    Elo will be in a saved data format along with the match history, with the wins, loses etc
+//
+//    Add a new set of pieces overlay for captured pieces which will be in the hotbar or on top probably
+//
+//
+//    Add an item in the bottom corner of the inventory, it will have z coordinates, so that it loads a gui texture (on top of the actual screen)
+//    All of the other items should load on top of that gui item. (Everything handled in datagen)
+//
+//    If wanting to be fancy we can add actual number textures. From 0 to 9
 }
